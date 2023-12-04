8ee5a6978afa0ae746d04dd77ceb4414d308c419
Dmytro <sagchik9@gmial.com>
2023-12-04 14:20:49 +0000

    data transfer objects setup

---
+++ ShopOnline.Models/Dtos/CartItemQtyUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Models.Dtos
+{
+    internal class CartItemQtyUpdateDto
+    {
+        public int CartItemId { get; set; }
+        public int Qty { get; set;}
+    }
+}
