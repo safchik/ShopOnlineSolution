f95e217017843a81968f84960e26457de5a8c815
Dmytro <sagchik9@gmial.com>
2023-12-04 16:09:05 +0000

    Dto conversion class created

---
+++ ShopOnline.Api/Extentions/DtoConversions.cs
@@ -0,0 +1,28 @@
+﻿using ShopOnline.Api.Entities;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Extentions
+{
+    public static class DtoConversions
+    {
+        public static IEnumerable<ProductDto> ConvertToDto(this IEnumerable<Product> products,
+            IEnumerable<ProductCategory> productCategories)
+        {
+            return (from product in products
+                    join ProductCategory in productCategories
+                    on product.CategoryId equals ProductCategory.Id
+                    select new ProductDto
+                    {
+                        Id = product.Id,
+                        Name = product.Name,
+                        Description = product.Description,
+                        ImageURL = product.ImageURL,
+                        Price = product.Price,
+                        Qty = product.Qty,
+                        CategoryId = product.CategoryId,
+                        CategoryName= ProductCategory.Name,
+                    }).ToList();
+        }
+        
+    }
+}
