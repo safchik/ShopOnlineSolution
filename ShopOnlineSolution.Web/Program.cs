52548cdf38da9b4345771526fd02e22dc94fa221
Dmytro <sagchik9@gmial.com>
2023-12-05 15:25:17 +0000

    setting up displaying product data to user

--- ShopOnlineSolution.Web/Program.cs
+++ ShopOnlineSolution.Web/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ShopOnlineSolution.Web;
+using ShopOnlineSolution.Web.Services;
+using ShopOnlineSolution.Web.Services.Contracts;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7145/") });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
---
+++ ShopOnlineSolution.Web/Services/Contracts/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using ShopOnline.Models.Dtos;
+
+namespace ShopOnlineSolution.Web.Services.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetItems();
+    }
+}
