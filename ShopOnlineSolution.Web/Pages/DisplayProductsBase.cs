a4b2c8ca6d0bd4f0b256fb7f2f4d9a659db8a22b
Dmytro <sagchik9@gmial.com>
2023-12-11 16:41:14 +0000

    product details razor component added

--- ShopOnlineSolution.Web/Pages/DisplayProductsBase.cs
+++ ShopOnlineSolution.Web/Pages/DisplayProductsBase.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿
+
+using Microsoft.AspNetCore.Components;
 using ShopOnline.Models.Dtos;
+using ShopOnlineSolution.Web.Services.Contracts;
 
 namespace ShopOnline.Web.Pages
 {
-    public class DisplayProductsBase:ComponentBase
+    public class ProductDetailsBase:ComponentBase
     {
         [Parameter]
-        public IEnumerable<ProductDto> Products { get; set; }
+        public int Id { get; set; }
 
+        [Inject]
+        public IProductService ProductService { get; set; }
+
+        public ProductDto Product { get; set; }
+        public string ErrorMessage { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Product = await ProductService.GetItem(Id);
+            }
+            catch (Exception ex)
+            {
+
+                ErrorMessage = ex.Message;
+            }
+        }
     }
 }
