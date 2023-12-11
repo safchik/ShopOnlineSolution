3fc05b80f0fc1c0aa714502f6e40b551787a31e8
Dmytro <sagchik9@gmial.com>
2023-12-11 16:17:28 +0000

    exeption handling functionallity within get items method

--- ShopOnlineSolution.Web/Services/Contracts/IProductService.cs
+++ ShopOnlineSolution.Web/Services/Contracts/IProductService.cs
@@ -5,5 +5,6 @@ namespace ShopOnlineSolution.Web.Services.Contracts
     public interface IProductService
     {
         Task<IEnumerable<ProductDto>> GetItems();
+        Task<ProductDto> GetItem(int id);
     }
 }
--- ShopOnlineSolution.Web/Services/ProductService.cs
+++ ShopOnlineSolution.Web/Services/ProductService.cs
@@ -13,16 +13,59 @@ namespace ShopOnlineSolution.Web.Services
             this.httpClient = httpClient;
         }
 
-        public async Task<IEnumerable<ProductDto>> GetItems()
+        public async Task<ProductDto> GetItem(int id)
         {
             try
             {
-                var products = await this.httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("api/Product");
-                return products;
+                var response = await httpClient.GetAsync($"api/Product/{id}");
+
+                if(response.IsSuccessStatusCode)
+                {
+                    if(response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        return default(ProductDto);
+                    }
+
+                    return await response.Content.ReadFromJsonAsync<ProductDto>();
+                }
+                else 
+                { 
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception(message);
+                }
             }
             catch (Exception)
             {
 
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetItems()
+        {
+            try
+            {
+                var response = await this.httpClient.GetAsync("api/Product");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        return Enumerable.Empty<ProductDto>();
+                    }
+
+                    return await response.Content.ReadFromJsonAsync<IEnumerable<ProductDto>>();
+                }
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"Http status code: {response.StatusCode} message: {message}");
+                }
+
+            }
+            catch (Exception)
+            {
+                //Log exception
                 throw;
             }
         }
