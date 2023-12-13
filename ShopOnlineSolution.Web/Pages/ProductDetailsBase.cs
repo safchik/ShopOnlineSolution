001cc2f72fd02ace9016ed6a77e0e8fa2d0271ce
Dmytro <sagchik9@gmial.com>
2023-12-13 17:33:36 +0000

    add item to cart function added

--- ShopOnlineSolution.Web/Pages/ProductDetailsBase.cs
+++ ShopOnlineSolution.Web/Pages/ProductDetailsBase.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Components;
 using ShopOnline.Models.Dtos;
+using ShopOnlineSolution.Web.Services.Contracts;
 
 namespace ShopOnline.Web.Pages
 {
     public class DisplayProductsBase : ComponentBase
     {
         [Parameter]
-        public IEnumerable<ProductDto> Products { get; set; }
+        public int Id { get; set; }
+
+        [Inject]
+        public IProductService ProductService { get; set; }
+
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+
+        
+        public ProductDto Product { get; set; }
+        public string ErrorMessage { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Product = await ProductService.GetItem(Id);
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+
+        protected async Task AddToCart_Click(CartItemToAddDto cartItemToAddDto)
+        {
+            try
+            {
+                var cartItemDto = await ShoppingCartService.AddItem(cartItemToAddDto);
+
+                
+            }
+            catch (Exception)
+            {
+
+                //Log Exception
+            }
+        }
 
     }
 }
